1ef3a9bb1954f6828961b93261098e2fa1acfead
jkhoang313 <jkhoang313@gmail.com>
2017-01-04 09:25:42 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,19 @@
 # Enter your procedural solution here!
+# def collect_multiples(limit)
+#   sum = 0
+#   current = 0
+#
+#   while current < limit
+#     sum += current if (current % 3 == 0) || (current % 5 ==0)
+#     current += 1
+#   end
+#   sum
+# end
+
+def collect_multiples(limit)
+  (1..limit-1).to_a.select {|number| number % 3 == 0 || number % 5 == 0}
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).inject {|sum, number| sum + number}
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,16 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    (1..limit-1).to_a.select {|number| number % 3 == 0 || number % 5 == 0}
+  end
+
+  def sum_multiples
+    collect_multiples.inject {|sum, number| sum + number}
+  end
+end
